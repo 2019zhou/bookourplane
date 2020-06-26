785addd92491613108292623f4fc970bc0fd6a0b
2019zhou <zhoujulia2019@outlook.com>
2020-06-26 22:58:49 +0800

    Add manager view logic

--- app.py
+++ app.py
@@ -115,8 +115,9 @@ def dashboard():
     else:
         return '登陆异常'
 
-    resultValue = cur.execute("SELECT ticket_id, seat_type, ticket.flight_id, departure_airport, destination_airport, departure_time,\
-                arrival_time  FROM ticket, flight WHERE passenger_id = %s and flight.flight_id = ticket.flight_id", user_id)
+    resultValue = cur.execute("SELECT ticket_id, seat_type, actual_fare, ticket.flight_id, departure_airport, destination_airport, departure_time,\
+                arrival_time  FROM ticket, flight WHERE passenger_id = %s and flight.flight_id = ticket.flight_id",
+                              user_id)
 
     if resultValue > 0:
         tickets = cur.fetchall()
@@ -162,7 +163,22 @@ def insert_data(seat_type, flight_id):
     else:
         return '登陆异常'
 
-    cur.execute("INSERT INTO ticket (passenger_id, flight_id, seat_type) VALUES(%s, %s,%s)",(user_id, flight_id, seat_type))
+    result = cur.execute("SELECT buytickets_discount FROM price_info, passenger WHERE passenger_id = %s \
+                         and price_info.passenger_type = passenger.type", user_id)
+    if result > 0:
+        discount = cur.fetchone()[0]
+    else:
+        return 'cannot fetchone desired discount'
+
+    result = cur.execute("SELECT ticket_price FROM ticket_info WHERE flight_id = %s and seat_type= %s",
+                         (flight_id, seat_type))
+    if result > 0:
+        ticket_price = cur.fetchone()[0]
+    else:
+        return 'cannot fetchone desired ticket_price'
+
+    cur.execute("INSERT INTO ticket (passenger_id, flight_id, seat_type, actual_fare) \
+                VALUES(%s, %s,%s,%s)", (user_id, flight_id, seat_type, ticket_price * discount))
 
     mysql.connection.commit()
     return redirect(url_for('search_login'))
@@ -174,6 +190,62 @@ def delete(id_data):
     cur.execute("DELETE FROM ticket WHERE ticket_id = %s", [id_data])
     mysql.connection.commit()
     return redirect(url_for('dashboard'))
+
+
+@app.route('/manager_login', methods=['GET', 'POST'])
+def manager_login():
+    if request.method == 'POST':
+        name = request.form['name']
+        password = request.form['password'].encode('utf-8')
+
+        curl = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        curl.execute("SELECT * FROM manager WHERE name=%s", (name,))
+        user = curl.fetchone()
+        curl.close()
+
+        if len(user) > 0:
+            if str(user["password"].encode('utf-8')) == str(password):
+                return redirect(url_for('manager_board'))
+            else:
+                return "Error password and email not match"
+        else:
+            return "Error user not found"
+    else:
+        return render_template("manage/manager_login.html")
+
+
+@app.route('/manager_board', methods=['GET', 'POST'])
+def manager_board():
+    cur = mysql.connection.cursor()
+    resultValue = cur.execute("SELECT * FROM flight")
+    if resultValue > 0:
+        flightDetails = cur.fetchall()
+
+    return render_template('manage/manager_board.html', flightDetails=flightDetails)
+
+
+@app.route('/update_flight', methods=['GET', 'POST'])
+def update_flight():
+    if request.method == 'POST':
+        flight_id = request.form['飞机编号']
+        dd = request.form['出发机场']
+        da = request.form['到达机场']
+        dt = request.form['出发时间']
+        at = request.form['到达时间']
+        cur = mysql.connection.cursor()
+        cur.execute("UPDATE flight \
+               SET departure_airport=%s, destination_airport=%s, departure_time=%s, \
+               arrival_time=%s WHERE flight_id=%s", (dd, da, dt, at, flight_id))
+        mysql.connection.commit()
+    return redirect(url_for('manager_board'))
+
+
+@app.route('/delete_flight/<string:flight_id>', methods=['GET', 'POST'])
+def delete_flight(flight_id):
+    cur = mysql.connection.cursor()
+    cur.execute("DELETE FROM flight WHERE flight_id = %s", [flight_id])
+    mysql.connection.commit()
+    return redirect(url_for('manager_board'))
 
 
 if __name__ == '__main__':
